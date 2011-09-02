1a3b313609f88c1f21412d6b3db0ac048767ad44
Simon Walker <s.r.walker101@googlemail.com>
2011-09-02 10:31:29 +0100

    added RAII fits object

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <stdexcept>
+#include <sqlitepp/sqlitepp.hpp>
+#include <fitsio.h>
+#include <tclap/CmdLine.h>
+
 
 using namespace std;
+
+struct Fits
+{
+    fitsfile *fptr;
+    int status;
+    Fits(const string &filename)
+        : status(0)
+    {
+        fits_open_file(&fptr, filename.c_str(), READWRITE, &status);
+    }
+
+    virtual ~Fits()
+    {
+        fits_close_file(fptr, &status);
+    }
+
+    void check()
+    {
+        if (status)
+        {
+            throw runtime_error("Error with fitsio");
+        }
+    }
+};
+
 
 int main(int argc, char *argv[])
 {
