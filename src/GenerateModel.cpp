878962e70f9f1e345c9f6bdffa87dcbe1cd0d709
Simon Walker <s.r.walker101@googlemail.com>
2011-09-02 17:27:55 +0100

    setting simulation constants in function

--- src/GenerateModel.cpp
+++ src/GenerateModel.cpp
@@ -84,6 +84,9 @@ vector<double> GenerateSynthetic(const vector<double> &jd, const Model &m)
     double normalisedDistance = m.a * AU / (m.rs * rSun);
     cout << "Normalisation constant: " << normalisedDistance << endl;
 
+    const double dr = 0.001;
+    const double dt = 60.;
+
     /* Package the coefficients into a vector */
     vector<double> coeffs(5);
     coeffs[1] = m.c1;
