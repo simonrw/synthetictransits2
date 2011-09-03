99b86c0689718a95d70f13f3c78ab74ba85465fb
Simon Walker <s.r.walker101@googlemail.com>
2011-09-03 15:09:48 +0100

    moved Fits object to another file

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@
 #include "timer.h"
 #include "GenerateModel.h"
 #include "Model.h"
+#include "FitsObject.h"
 
 
 
@@ -19,97 +20,7 @@ using namespace std;
 using namespace sqlitepp;
 
 
-class Fits
-{
-    public:
-        Fits(const string &filename)
-            : m_status(0), m_filename(filename)
-        {
-            fits_open_file(&this->m_fptr, this->m_filename.c_str(), READWRITE, &this->m_status);
-            this->check();
-        }
 
-
-        virtual ~Fits()
-        {
-            fits_close_file(this->m_fptr, &this->m_status);
-            this->check();
-        }
-
-        void check()
-        {
-            if (this->m_status)
-            {
-                char buf[FLEN_STATUS];
-                fits_get_errstatus(this->m_status, buf);
-                throw runtime_error(buf);
-            }
-        }
-
-        void moveHDU(const string &hduname)
-        {
-            fits_movnam_hdu(this->m_fptr, ANY_HDU, const_cast<char*>(hduname.c_str()), 0, &this->m_status);
-            this->check();
-        }
-
-        void moveHDU(int hdunum)
-        {
-            int hdutype;
-            fits_movabs_hdu(this->m_fptr, hdunum, &hdutype, &this->m_status);
-            this->check();
-        }
-
-        fitsfile **fptr() { return &this->m_fptr; }
-        int &status() { return this->m_status; }
-
-        static void check(int status)
-        {
-            if (status)
-            {
-                char buf[FLEN_STATUS];
-                fits_get_errstatus(status, buf);
-                throw runtime_error(buf);
-            }
-        }
-
-        const string hduname()
-        {
-            char buf[FLEN_VALUE];
-            fits_read_key(this->m_fptr, TSTRING, "EXTNAME", buf, NULL, &this->m_status);
-            this->check();
-            return string(buf);
-        }
-
-
-
-    protected:
-        fitsfile *m_fptr;
-        int m_status;
-        string m_filename;
-
-        /* Default constructor - does nothing */
-        Fits() {}
-
-
-};
-
-class NewFits : public Fits
-{
-    public:
-    NewFits(const string &filename)
-    {
-        this->m_filename = filename;
-        this->m_status = 0;
-
-        fits_create_file(&this->m_fptr, filename.c_str(), &this->m_status);
-        this->check();
-
-        /* Ensure the basic keywords are there */
-        long naxes[] = {0, 0};
-        fits_create_img(this->m_fptr, BYTE_IMG, 0, naxes, &this->m_status);
-        this->check();
-    }
-};
 
 long indexOf(const vector<string> &stringlist, const string &comp)
 {
