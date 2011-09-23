7813182ace2933fa09ef58487856cf906e82931a
Simon Walker <s.r.walker101@googlemail.com>
2011-09-23 10:08:10 +0100

    no longer printing ratio parameter

--- Check.py
+++ Check.py
@@ -126,7 +126,7 @@ def main(args):
         axvline(JWWidth/2., color='g', label="JW")
 
         # print the ratio of widths
-        print "Ratio: %f" % (((Widths[i] / 2.) / (PJWWidth/2.)) / CurrentModel['period'],)
+        #print "Ratio: %f" % (((Widths[i] / 2.) / (PJWWidth/2.)) / CurrentModel['period'],)
 
         xlim(-0.3, 0.3)
         ylim(0.5, 1.5)
